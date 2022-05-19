d09dc6ebc3f2921503f754db1d8dd2937e193784
NGERANIS\Student <nathangeranis@outlook.com>
2022-05-18 22:16:19 -0500

    Added Blazorise

--- Borealis.Web/Global.cs
+++ Borealis.Web/Global.cs
@@ -2,6 +2,9 @@
 global using System.Reflection;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.AspNetCore.Components.Web;
+global using Blazorise;
+global using Blazorise.Bootstrap5;
+global using Blazorise.Icons.FontAwesome;
 global using Radzen;
 global using Borealis.Web;
 global using Borealis.Web.Utilities;
